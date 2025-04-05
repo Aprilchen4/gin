990652eb1e3a1bab7b378273692d29e336e0a7fe
Aprilchen4 <15991698448@163.com>
2025-04-05 21:56:45 +0800

    loginView

--- src/views/loginWeb.vue
+++ src/views/loginWeb.vue
@@ -1,121 +1,95 @@
 <template>
-  <body class="page">
-    <div class="login-container">
-      <div class="content">
-        <!-- 左上 -->
-        <div class="login-title">
-          <el-image
-            class="logo"
-            :src="require('@/assets/logo1.png')"
-          ></el-image>
-          <h2 class="word1">Gin-Vue-Admin</h2>
-          <div class="word2">A management platform using Golang and Vue</div>
-        </div>
-        <!-- 左中 -->
-        <div class="login-box">
-          <!--"loginForm" 是DOM组件实例的引用名称，开发者自定义的名称-->
-          <!-- 通过 this.$refs.loginForm，可以在 Vue.js 代码中直接访问并操作这个表单组件实例。  -->
-          <!-- 比如,可以通过 this.$refs.loginForm.focus() 来让表单组件获得焦点。
+  <!-- <body class="page"> -->
+  <div class="login-container">
+    <div class="content">
+      <!-- 左上 -->
+      <div class="login-title">
+        <el-image class="logo" :src="require('@/assets/logo1.png')"></el-image>
+        <h2 class="word1">Gin-Vue-Admin</h2>
+        <div class="word2">A management platform using Golang and Vue</div>
+      </div>
+      <!-- 左中 -->
+      <div class="login-box">
+        <!--"loginForm" 是DOM组件实例的引用名称，开发者自定义的名称-->
+        <!-- 通过 this.$refs.loginForm，可以在 Vue.js 代码中直接访问并操作这个表单组件实例。  -->
+        <!-- 比如,可以通过 this.$refs.loginForm.focus() 来让表单组件获得焦点。
             或者通过 this.$refs.loginForm.validate() 来手动触发表单验证。 -->
-          <!-- <el-form :model="form" :rules="rules" ref="loginForm"> 原版，下行不加loginForm也可以-->
-          <el-form :model="form" :rules="rules">
-            <!-- prop 是 Element UI 表单组件特有的一个属性绑定 -->
-            <!-- 使用了 prop 属性进行绑定，"username" 是表单数据模型中的一个字段名称 -->
-            <!-- 为了将这个表单项与表单数据模型中的 username 字段进行关联，让表单验证功能能够正常工作， -->
-            <!-- 表单验证规则会根据 prop 属性来应用到对应的表单字段上 -->
-            <el-form-item prop="username">
-              <!-- v-model（数据）和prop（字段）不同，输入值直接绑定到了 form.username 这个数据属性上-->
-              <!-- 一定是 form.username，仅password没关联上，代码不知道你password是哪个-->
-              <el-input
-                v-model="form.username"
-                placeholder="请输入用户名"
-                :suffix-icon="User"
-                clearable
-              >
-                <!-- 好神奇的写法 :suffix-icon="User" 不可缺  -->
-                <!-- <template #suffix> -->
-                <el-icon><User /></el-icon>
-                <!-- </template> -->
-              </el-input>
-            </el-form-item>
-            <el-form-item prop="password">
-              <el-input v-model="form.password" placeholder="请输入密码">
-              </el-input>
-            </el-form-item>
-            <el-form-item prop="usercode">
-              <div class="code">
-                <div>
-                  <el-input v-model="form.captcha" placeholder="请输入验证码" />
-                </div>
-                <!-- 这里v-model要和script-data/setup的数据对应，否则报错 -->
-                <!-- <div><VerifyCode/></div> -->
-                <!-- v-if="picPath" 只有当 picPath 存在时，才会渲染 <img> 标签 -->
-                <!--picPath 是 Vue 组件的一个变量，它存储了图片的路径或 Base64 数据，并绑定到 src 属性上 -->
-                <img
-                  v-if="picPath"
-                  :src="picPath"
-                  style="width: 80px; height: 40px"
-                />
-                <!-- 不在这里修改验证码格式 -->
+        <!-- <el-form :model="form" :rules="rules" ref="loginForm"> 原版，下行不加loginForm也可以-->
+        <el-form :model="form" :rules="rules">
+          <!-- prop 是 Element UI 表单组件特有的一个属性绑定 -->
+          <!-- 使用了 prop 属性进行绑定，"username" 是表单数据模型中的一个字段名称 -->
+          <!-- 为了将这个表单项与表单数据模型中的 username 字段进行关联，让表单验证功能能够正常工作， -->
+          <!-- 表单验证规则会根据 prop 属性来应用到对应的表单字段上 -->
+          <el-form-item prop="username">
+            <!-- v-model（数据）和prop（字段）不同，输入值直接绑定到了 form.username 这个数据属性上-->
+            <!-- 一定是 form.username，仅password没关联上，代码不知道你password是哪个-->
+            <el-input v-model="form.username" placeholder="请输入用户名" :suffix-icon="User" clearable>
+              <!-- 好神奇的写法 :suffix-icon="User" 不可缺  -->
+              <!-- <template #suffix> -->
+              <el-icon><User /></el-icon>
+              <!-- </template> -->
+            </el-input>
+          </el-form-item>
+          <el-form-item prop="password">
+            <el-input v-model="form.password" placeholder="请输入密码"> </el-input>
+          </el-form-item>
+          <el-form-item prop="usercode">
+            <div class="code">
+              <div>
+                <el-input v-model="form.captcha" placeholder="请输入验证码" />
               </div>
-            </el-form-item>
-          </el-form>
-        </div>
-        <!-- 左下 -->
-        <el-form-item>
-          <div class="button">
-            <!-- 一点点对不齐，源于自带的位移，所以单独加div -->
-            <div>
-              <el-button type="primary" @click="fetchData">登录</el-button>
+              <!-- 这里v-model要和script-data/setup的数据对应，否则报错 -->
+              <!-- <div><VerifyCode/></div> -->
+              <!-- v-if="picPath" 只有当 picPath 存在时，才会渲染 <img> 标签 -->
+              <!--picPath 是 Vue 组件的一个变量，它存储了图片的路径或 Base64 数据，并绑定到 src 属性上 -->
+              <img v-if="picPath" :src="picPath" style="width: 80px; height: 40px" />
+              <!-- 不在这里修改验证码格式 -->
             </div>
-            <router-view></router-view>
-            <div><el-button type="primary">前往初始化</el-button></div>
+          </el-form-item>
+        </el-form>
+      </div>
+      <!-- 左下 -->
+      <el-form-item>
+        <div class="button">
+          <!-- 一点点对不齐，源于自带的位移，所以单独加div -->
+          <div>
+            <el-button type="primary" @click="fetchData">登录</el-button>
           </div>
-        </el-form-item>
-      </div>
-      <!-- right还是要在login-container里面，否则不符合左右排列 -->
-      <div class="right">
-        <el-image
-          class="picture-line"
-          :src="require('@/assets/line.png')"
-        ></el-image>
-        <el-image
-          class="picture"
-          :src="require('@/assets/hello world.jpg')"
-        ></el-image>
-        <el-image
-          class="picture-fill"
-          :src="require('@/assets/fill.png')"
-        ></el-image>
-        <!-- 最右侧否则填充不完全 -->
-      </div>
+          <router-view></router-view>
+          <div><el-button type="primary">前往初始化</el-button></div>
+        </div>
+      </el-form-item>
     </div>
-    <div class="bottom">
-      <div>Powered by</div>
-      <a href="https://github.com/flipped-aurora/gin-vue-admin" class="bottom-a"
-        >Gin-Vue-Admin</a
-      >
-      <div class="icon-link">
-        <!-- 颜色写在上一行里面，不行 -->
-        <a href="https://www.gin-vue-admin.com/"
-          ><el-icon color="#150202"><Document /></el-icon
-        ></a>
-        <a href="https://support.qq.com/product/371961"
-          ><el-icon color="#150202"><Service /></el-icon
-        ></a>
-        <a href="https://github.com/flipped-aurora/gin-vue-admin"
-          ><el-icon color="#150202"><Present /></el-icon
-        ></a>
-        <a href="https://space.bilibili.com/322210472"
-          ><el-icon color="#150202"><Microphone /></el-icon
-        ></a>
-      </div>
-      <div>Copyright</div>
-      <a href="https://github.com/flipped-aurora" class="bottom-a"
-        >flipped-aurora团队</a
-      >
+    <!-- right还是要在login-container里面，否则不符合左右排列 -->
+    <div class="right">
+      <el-image class="picture-line" :src="require('@/assets/line.png')"></el-image>
+      <el-image class="picture" :src="require('@/assets/hello world.jpg')"></el-image>
+      <el-image class="picture-fill" :src="require('@/assets/fill.png')"></el-image>
+      <!-- 最右侧否则填充不完全 -->
     </div>
-  </body>
+  </div>
+  <div class="bottom">
+    <div>Powered by</div>
+    <a href="https://github.com/flipped-aurora/gin-vue-admin" class="bottom-a">Gin-Vue-Admin</a>
+    <div class="icon-link">
+      <!-- 颜色写在上一行里面，不行 -->
+      <a href="https://www.gin-vue-admin.com/"
+        ><el-icon color="#150202"><Document /></el-icon
+      ></a>
+      <a href="https://support.qq.com/product/371961"
+        ><el-icon color="#150202"><Service /></el-icon
+      ></a>
+      <a href="https://github.com/flipped-aurora/gin-vue-admin"
+        ><el-icon color="#150202"><Present /></el-icon
+      ></a>
+      <a href="https://space.bilibili.com/322210472"
+        ><el-icon color="#150202"><Microphone /></el-icon
+      ></a>
+    </div>
+    <div>Copyright</div>
+    <a href="https://github.com/flipped-aurora" class="bottom-a">flipped-aurora团队</a>
+  </div>
+  <!-- </body> -->
 </template>
 
 <script>
@@ -125,6 +99,7 @@ import { ElInput, ElButton } from "element-plus";
 import { ref } from "vue";
 import { getCode, getlogin } from "@/api/user";
 //import { router } from '@/router'
+// import { nextTick } from "vue";
 
 // useRouter 只能在 setup 函数或 <script setup> 中使用，是一个局部工具函数
 // 每次在组件中使用 useRouter，都会返回当前的路由实例，即 router.js 中导出的 router
@@ -182,8 +157,10 @@ export default {
         console.log("登录非常成功:", response);
 
         // 编程式导航
-        // router.push('/menu')
-        router.push({ path: "/ginmenu" });
+        // router.push({ path: "/ginmenu" });
+        router.push("/ginmenu").then(() => {
+          window.location.reload(); // 强制刷新（不推荐长期使用）
+        });
 
         // 假设返回的数据中有 token，存储在 localStorage
         // response.token 是响应对象中的一个字段，表示用户的身份验证令牌（Token）
@@ -249,8 +226,18 @@ export default {
 </script>
 
 <style>
+html,
+body,
+#app {
+  /* 确保 Vue 根容器也继承高度 */
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  overflow: hidden; /* 锁定全局滚动条 */
+}
+
 .page {
-  width: 100%;
+  width: 100vw; /* 使用视口单位代替百分比 */
   height: 100vh;
   /* 这么写老实了，不滚动了 */
   overflow: hidden;
@@ -258,17 +245,20 @@ export default {
 .login-container {
   /* overflow: hidden; 写这里没用，要写在父级 */
   display: flex;
-  flex-direction: raw;
+  flex-direction: row;
 }
 
 .content {
   display: flex;
   flex-direction: column;
+  justify-content: center; /* 水平居中 */
   align-items: center;
   text-align: center;
   /* 垂直居中 */
+  margin-bottom: 100px;
   margin-left: 160px;
   width: 600px;
+  min-width: 40%;
 }
 
 .login-title {
@@ -307,7 +297,7 @@ export default {
 /* margin-bottom: 20px;  */
 /* 这么写避免了提示错误的预留空间过大 */
 /* padding:10px; */
-/* 这个不是文字的规范  
+/* 这个不是文字的规范
   } */
 
 .code {
@@ -335,22 +325,31 @@ export default {
 
 .right {
   display: flex;
-  justify-content: center;
-  /* 实现了左右分布 */
+  align-items: center; /* 垂直居中 */
+  height: 100vh;
+  position: relative; /* 用于固定 picture 和 picture-fill */
+  overflow: hidden; /* 防止内容溢出 */
 }
 
 .picture-line {
-  width: 300px;
-  height: 100vh;
+  height: 100vh; /* 高度填满 */
+  width: auto; /* 宽度自适应 */
+  min-width: 100px; /* 最小宽度防止过小 */
+  object-fit: cover;
+  flex-shrink: 1; /* 允许缩放 */
 }
 
 .picture {
-  width: 80%;
+  width: 60%;
   height: 100vh;
+  object-fit: cover;
 }
 
 .picture-fill {
+  width: 20%;
   height: 100vh;
+  object-fit: cover;
+  /* flex-shrink: 0; 禁止缩放 */
 }
 
 .bottom {
